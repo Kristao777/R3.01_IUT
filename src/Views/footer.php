490df1650f197094a8129420e53927980ac7175a
kristao <kristao777@gmail.com>
2024-09-15 22:13:47 +0200

    modifs

--- src/Views/footer.php
+++ src/Views/footer.php
@@ -1,3 +1,4 @@
+    </div>
 <footer>
-<div class="position-absolute bottom-0 start-50 translate-middle-x">&copy; 2024 - IUT</div>
+    <div class="position-absolute bottom-0 start-50 translate-middle-x">&copy; 2024 - IUT</div>
 </footer>
